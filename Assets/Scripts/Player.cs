f1427a63dc391502a7cc06b1a0d43d6ce0144ccd
kitajima2910 <kitajima2910@gmail.com>
2024-09-04 18:02:14 +0700

    - Phan 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    [SerializeField] float speed = 20f;
+    private float horizontalInput;
+
+    // Jump
+    [SerializeField] float jumpForce = 100f;
+    private bool isGrounded = false;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + horizontalMove + forwardMove);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = true;
+        }
+    }
+
+    private void oncollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGrounded = false;
+        }
+    }
+
+    void Jump()
+    {
+        animator.SetTrigger("Jump");
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+}
